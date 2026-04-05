141780d29e6af0cafa17bced7f3412504324f0fd
mrgattax <33103275+lucasgattas@users.noreply.github.com>
2026-04-05 19:33:03 -0300

    Refactor NAGUNetModel forward method and context handling

--- sd/openaimodel.py
+++ sd/openaimodel.py
@@ -1,7 +1,6 @@
 from functools import partial
 from types import MethodType
 
-import torch
 import comfy
 from comfy.ldm.modules.attention import CrossAttention
 from comfy.ldm.modules.diffusionmodules.openaimodel import UNetModel
@@ -12,40 +11,44 @@ from ..utils import cat_context, check_nag_activation, NAGSwitch
 
 class NAGUNetModel(UNetModel):
     def forward(
-            self,
-            x,
-            timesteps=None,
-            context=None,
-            y=None,
-            control=None,
-            transformer_options={},
-
-            nag_negative_context=None,
-            nag_sigma_end=0.,
-
-            **kwargs,
+        self,
+        x,
+        timesteps=None,
+        context=None,
+        y=None,
+        control=None,
+        transformer_options={},
+        nag_negative_context=None,
+        nag_sigma_end=0.0,
+        **kwargs,
     ):
         apply_nag = check_nag_activation(transformer_options, nag_sigma_end)
-        if apply_nag:
-            context = cat_context(context, nag_negative_context)
-            cross_attns_forward = list()
-            for name, module in self.named_modules():
-                if "attn2" in name and isinstance(module, CrossAttention):
-                    cross_attns_forward.append((module, module.forward))
-                    module.forward = MethodType(NAGCrossAttention.forward, module)
 
-        output = comfy.patcher_extension.WrapperExecutor.new_class_executor(
-            self._forward,
-            self,
-            comfy.patcher_extension.get_all_wrappers(comfy.patcher_extension.WrappersMP.DIFFUSION_MODEL,
-                                                     transformer_options)
-        ).execute(x, timesteps, context, y, control, transformer_options, **kwargs)
+        patched_modules = []
 
-        if apply_nag:
-            for mod, forward_fn in cross_attns_forward:
+        try:
+            if apply_nag:
+                context = cat_context(context, nag_negative_context)
+
+                for name, module in self.named_modules():
+                    if "attn2" in name and isinstance(module, CrossAttention):
+                        patched_modules.append((module, module.forward))
+                        module.forward = MethodType(NAGCrossAttention.forward, module)
+
+            output = comfy.patcher_extension.WrapperExecutor.new_class_executor(
+                self._forward,
+                self,
+                comfy.patcher_extension.get_all_wrappers(
+                    comfy.patcher_extension.WrappersMP.DIFFUSION_MODEL,
+                    transformer_options,
+                ),
+            ).execute(x, timesteps, context, y, control, transformer_options, **kwargs)
+
+            return output
+
+        finally:
+            for mod, forward_fn in patched_modules:
                 mod.forward = forward_fn
-
-        return output
 
 
 class NAGUNetModelSwitch(NAGSwitch):
@@ -56,8 +59,9 @@ class NAGUNetModelSwitch(NAGSwitch):
                 nag_negative_context=self.nag_negative_cond[0][0],
                 nag_sigma_end=self.nag_sigma_end,
             ),
-            self.model
+            self.model,
         )
+
         for name, module in self.model.named_modules():
             if "attn2" in name and isinstance(module, CrossAttention):
                 module.nag_scale = self.nag_scale
